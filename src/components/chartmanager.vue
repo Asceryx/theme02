383ab124dfdc0993a751b81b62ddce612cf6ff83
mael-ramillien <maelramillien@yahoo.fr>
2020-10-22 18:17:23 +0200

    correction

--- src/components/chartmanager.vue
+++ src/components/chartmanager.vue
@@ -8,7 +8,7 @@
     <div v-if="selected === 'line-chart'">
       <line-chart 
         id="line" 
-        :data="data"
+        :data="dataValue"
         xkey="date"
         resize="true"
         ykeys='["value"]'
@@ -22,7 +22,7 @@
     <div v-if="selected === 'bar-chart'">
       <bar-chart 
         id="bar" 
-        :data="data" 
+        :data="dataValue" 
         xkey="date" 
         ykeys='[ "value" ]' 
         resize="true"
@@ -36,7 +36,7 @@
     <div v-if="selected === 'area-chart'">
       <area-chart
         id="area"
-        :data="data"
+        :data="dataValue"
         xkey="date" 
         ykeys='[ "value" ]' 
         line-colors='[ "#FF6384"]' 
@@ -50,7 +50,8 @@
 
 <script>
 import { LineChart, BarChart, AreaChart } from 'vue-morris'
-//import axios from 'axios';
+import axios from 'axios';
+import moment from 'moment';
 
 export default {
   name: 'chartmanager',
@@ -59,7 +60,7 @@ export default {
     return {
       selected : "line-chart",
       error : false,
-      data: [],
+      dataValue: [],
       charts : [
         'line-chart',
         'bar-chart',
@@ -69,45 +70,30 @@ export default {
   },
   methods: {
     jsonToObject(jsonData) {
-      return jsonData.map(data => this.parseData(data));
+      jsonData.data.forEach(element => {
+        this.dataValue.push({date: moment(element.start).format('YYYY-MM-DD'), value: this.parseData(element)});
+      });
     },
 
     interval(startTime, endTime) {
-      const dt1 = new Date(endTime).format('YYYY-MM-DD hh:mm:ss') 
-      const dt2 = new Date(startTime).format('YYYY-MM-DD hh:mm:ss');
-      var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+      const dt1 = moment(endTime).unix();
+      const dt2 = moment(startTime).unix();
+      var diff = (parseInt(dt2) - parseInt(dt1));
       diff /= (60 * 60);
-      return Math.abs(Math.round(diff));
+      return diff;
     },
 
     parseData(data) {
-      const startDate = new Date(data.start).format('YYYY-MM-DD hh:mm:ss')
       return { 
-        date: startDate.getYear() + '-' + startDate.getMonth() + '-' + startDate.getDay(),
+        date: moment(data.start).format('YYYY-MM-DD'),
         value: this.interval(data.start, data.end)
       }
     },
 
     retrieveWorktimes () {
-      // axios
-      // .get("", {params: {user_id: this.userid}})
-      // .then(response => this.data = this.jsonToObject(response), this.error=true);
-      this.data = [
-        { date: '2019-08-01', value: 20},
-        { date: '2019-08-02', value: 15},
-        { date: '2019-08-03', value: 23},
-        { date: '2019-08-04', value: 13},
-        { date: '2019-08-05', value: 12}, 
-        { date: '2019-08-06', value: 10}, 
-        { date: '2019-08-07', value: 11}, 
-        { date: '2019-08-08', value: 15}, 
-        { date: '2019-08-09', value: 6},
-        { date: '2019-08-10', value: 3}, 
-        { date: '2019-08-11', value: 30}, 
-        { date: '2019-08-12', value: 4}, 
-        { date: '2019-08-13', value: 1}, 
-        { date: '2019-08-14', value: 2}, 
-      ]
+      axios
+      .get("/api/workingtimes/" + this.userid)
+      .then(response => this.data = this.jsonToObject(response.data), this.error=true);
     }
   },
   components: { 
